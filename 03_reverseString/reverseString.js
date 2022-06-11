afac3ec115b8df8b1c13f5f4e98b932d6c7d6b2c
JustWaveThings <flove101@gmail.com>
2022-06-10 22:14:58 -0400

    reverseString passed all 4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+    const fwdString = string.split('');
+    const rewString = fwdString.reverse();
+    let result = rewString.join('');
+    return result
 };
+
+// psuedocode
+// take string and make array with element being each letter
+// then reverse array
+// then array.toString() - should work
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
