478d3b23d9d46995f4452f3703ab988d79036424
JustWaveThings <flove101@gmail.com>
2022-06-11 12:56:19 -0400

    removeFromArray.js fleshed out pseudocode and the code for handling the first input parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,5 +2,26 @@ const removeFromArray = function() {
 
 };
 
+
+
+//From Exercise :  Implement a function that takes an array and some other arguments then removes the other arguments from that array:
+
+
+
+// there are two types of input in this function 
+// -- first the array to be worked on 
+//      -- per test spec must work on strings and arrays (test 6)
+//          -- should have a test to convert input string to array if not array
+
+
+// -- variable amount of optional arguments that need to be applied to the first array
+//          -- per last test,  we only want to work on the same type (don't use a method that removes all instances, only the first instance (last test))
+
+// 
+
+
+
+
+
 // Do not edit below this line
 module.exports = removeFromArray;
