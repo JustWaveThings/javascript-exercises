b43a78fdf5e36aec0caffc4c45b80275568b81a0
JustWaveThings <flove101@gmail.com>
2022-06-14 01:04:17 -0400

    leapYears exercise passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = (years) => {
+    if ((years % 400) === 0) return true;
+    if ((years % 4) === 0 && (years % 100) !== 0) return true;  
+    return false;
 };
+
+//pseudocode 
+// && (years % 100) === !0)
+// need to see if input is divisible by 4 or 400, but not 100
 
 // Do not edit below this line
 module.exports = leapYears;
