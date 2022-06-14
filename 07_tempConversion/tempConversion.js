e4aa12c439b09fe77ed1111d8efbed2a157178d0
JustWaveThings <flove101@gmail.com>
2022-06-14 01:35:38 -0400

    tempConversion exercise passes all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = (f) => {
+  // [°C] = ([°F] − 32) × 5⁄9
+  let celcius = ((f - 32) * (5 / 9));
+  return parseFloat(decimalToOnePlace(celcius));
 };
 
-const ctof = function() {
-
+const ctof = (c) => {
+  // [°F] = [°C] × 9⁄5 + 32
+  let fahrenheit = (c * (9 / 5) + 32);
+  return parseFloat(decimalToOnePlace(fahrenheit));
 };
+
+function decimalToOnePlace(num) {
+  return Number.parseFloat(num).toFixed(1);
+}
 
 // Do not edit below this line
 module.exports = {
