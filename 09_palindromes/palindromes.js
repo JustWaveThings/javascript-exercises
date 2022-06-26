2211abd7c89f90e2a0d52e9bd799a35fb31bb681
JustWaveThings <flove101@gmail.com>
2022-06-26 13:19:07 -0400

    Palindromes tests pass and solution reviewed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,30 @@
 
-
-const palindromes = (phrase) => {
+const myPalindromes = phrase => {
     const regex = /[A-Za-z]/g;
-    const newPhrase = phrase.match(regex); 
-    const phraseLower = newPhrase.map(element => {
-        return element.toLowerCase();
+    const onlyLetters = phrase.match(regex); 
+    const onlyLettersLower = onlyLetters.map(element => {
+        return element.toLowerCase(); 
     });
-    const reversePhrase = phraseLower.reverse();
-    console.log(phraseLower);
-    console.log(phraseLower.reverse());
-    return (phraseLower === reversePhrase) ? true : false;
-}; 
-palindromes("ZZZZ car, a man, a maracaz.");
+    const onlyLettersLowerCopy = onlyLettersLower.map((x) => x); 
+    const reverseOnlyLettersLowerCopy = onlyLettersLowerCopy.reverse(); 
+    function arrayEquals(a, b) {
+        return a.every((val, index) => val === b[index]);
+    }
+    return arrayEquals(onlyLettersLower, reverseOnlyLettersLowerCopy);
+}
+
+// WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW versus given solution WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+
+const palindromes = function (string) {
+    processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
+};
+
+palindromes("Just wave Things ..."); // false
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
