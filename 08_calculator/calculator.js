396ec1609c5f6b5c8ff783f9550924badcfbdb4d
JustWaveThings <flove101@gmail.com>
2022-06-25 23:33:09 -0400

    JS Exercises Calculator - all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) =>  num1 - num2;
+
+
+
+const sum = array => {
+  let total = 0;
+  for (const arg of array) {
+    total += arg;
+  }
+  return total;
+}
+
+array = [2, 4, 6, 8, 10, 12, 14];
+
+const multiply = array => {
+  total = 1;
+  for (const f of array) {
+    total *= f;
+  }
+  return total;
+};
+
+const power = (num1, num2) => num1 ** num2;
+
+const factorial = number => {
+  let total = 1;
+  for (let i = number; i >= 1; i--) {
+    total *= i;
+  }
+  return total;
+};
 	
-};
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
