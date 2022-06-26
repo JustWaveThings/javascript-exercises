985e0b387e6bb81ddb0c43b0cc2d3193bad6da04
JustWaveThings <flove101@gmail.com>
2022-06-26 00:43:46 -0400

    Palindrome conditional not evaluating correctly, but rest works. confirm with console.log

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
 
-};
+
+const palindromes = (phrase) => {
+    const regex = /[A-Za-z]/g;
+    const newPhrase = phrase.match(regex); 
+    const phraseLower = newPhrase.map(element => {
+        return element.toLowerCase();
+    });
+    const reversePhrase = phraseLower.reverse();
+    console.log(phraseLower);
+    console.log(phraseLower.reverse());
+    return (phraseLower === reversePhrase) ? true : false;
+}; 
+palindromes("ZZZZ car, a man, a maracaz.");
 
 // Do not edit below this line
 module.exports = palindromes;
